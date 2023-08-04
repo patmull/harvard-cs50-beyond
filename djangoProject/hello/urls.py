94246a27a98832815d039f51820242b70ea2aab8
patmull <patmull@seznam.cz>
2023-08-04 20:19:15 +0200

    Added css

--- djangoProject/hello/urls.py
+++ djangoProject/hello/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path("brian", views.brian, name="brian"),
 
     # Better: using parameter
-    path("greet/<str:name>", views.greet, name="greet")
+    path("greet/<str:name>", views.greet, name="greet"),
+    path("greet-better/<str:name>", views.greet_better, name="greet_better"),
+    path("greet-even-better/<str:name>", views.greet_even_better, name="greet_even_better"),
+    path('isitchristmas', views.is_christmas, name="is_christmas")
 ]
--- djangoProject/hello/views.py
+++ djangoProject/hello/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -12,4 +14,29 @@ def brian(request):
 
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name}!")
+    return HttpResponse(f"Hello, {name.capitalize()}!")
+
+
+def greet_better(request, name):
+    return HttpResponse(f"<h1>Hello, {name.capitalize()}!</h1>")
+
+
+def greet_even_better(request, name):
+    # FLASK: return render_template('templates/greet.html', name=name.capitalize())
+    # Django:
+    return render(request, 'greet.html', {
+        "name": name.capitalize()
+    })
+
+
+def is_christmas(request):
+    date = datetime.now()
+    is_it_christmas_returned_value = False
+    if 23 < date.day < 27 and date.month == 12:
+        is_it_christmas_returned_value = True
+    else:
+        is_it_christmas_returned_value = False
+
+    return render(request, 'hello/is-it.christmas.html', {
+        "is_christmas_now": is_it_christmas_returned_value
+    })
