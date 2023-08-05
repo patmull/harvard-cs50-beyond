c7f43bafe6f8809d506e2bb737223cdbd25d8eaf
patmull <patmull@seznam.cz>
2023-08-05 16:25:27 +0200

    Added form programmatically

--- djangoProject/tasks/views.py
+++ djangoProject/tasks/views.py
@@ -1,8 +1,13 @@
+from django import forms
 from django.shortcuts import render
 
 # If lists is defined as a "global" variable, values stays there until server restart
 LIST_OF_TASKS = ['nákup', 'vysávání', 'zřídit účet']
 LIST_OF_SHOPPING_ITEMS = []
+
+
+class NewItemForm(forms.Form):
+    new_list_item = forms.CharField(label="Item:", required=True)
 
 
 # create your views here.
@@ -12,11 +17,12 @@ def index(request):
     # If lists are defined as a function variables, values are lost after page refresh
     # list_of_tasks = ['nákup', 'vysávání', 'zřídit účet']
     if request.method == "POST":
-        new_task_name = request.POST.get('task_name')
+        new_task_name = request.POST.get('new_list_item')
         list_of_tasks.append(new_task_name)
 
     return render(request, 'tasks.html', {
-        'tasks': list_of_tasks
+        'tasks': list_of_tasks,
+        "new_item_form": NewItemForm
     })
 
 
@@ -26,9 +32,10 @@ def shopping_lists(request):
     # If lists are defined as a function variables, values are lost after page refresh
     # list_of_tasks = ['nákup', 'vysávání', 'zřídit účet']
     if request.method == "POST":
-        new_shopping_item_name = request.POST.get('shopping_item_name')
+        new_shopping_item_name = request.POST.get('new_list_item')
         list_of_shopping_items.append(new_shopping_item_name)
 
     return render(request, 'shopping-lists.html', {
-        'shopping_items': list_of_shopping_items
+        'shopping_items': list_of_shopping_items,
+        "new_item_form": NewItemForm
     })
