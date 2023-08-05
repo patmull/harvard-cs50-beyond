6ed6c57c91f81adda5bee0fbc3f4ba3829df0763
patmull <patmull@seznam.cz>
2023-08-05 16:08:28 +0200

    Added new form to demonstrate that duplicating forms and html is not good practise

--- djangoProject/tasks/urls.py
+++ djangoProject/tasks/urls.py
@@ -4,5 +4,7 @@ from tasks import views
 
 urlpatterns = [
     path('', views.index, name='tasks'),
-    path('new-task', views.index, name='new_task')
+    path('shopping-lists', views.shopping_lists, name='shopping_lists'),
+    path('new-task', views.index, name='new_task'),
+    path('new-shopping-item',  views.shopping_lists, name='add_new_shopping_item')
 ]
--- djangoProject/tasks/views.py
+++ djangoProject/tasks/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # If lists is defined as a "global" variable, values stays there until server restart
 LIST_OF_TASKS = ['nákup', 'vysávání', 'zřídit účet']
+LIST_OF_SHOPPING_ITEMS = []
 
 
 # create your views here.
@@ -16,4 +17,18 @@ def index(request):
 
     return render(request, 'tasks.html', {
         'tasks': list_of_tasks
+    })
+
+
+def shopping_lists(request):
+    list_of_shopping_items = LIST_OF_SHOPPING_ITEMS
+
+    # If lists are defined as a function variables, values are lost after page refresh
+    # list_of_tasks = ['nákup', 'vysávání', 'zřídit účet']
+    if request.method == "POST":
+        new_shopping_item_name = request.POST.get('shopping_item_name')
+        list_of_shopping_items.append(new_shopping_item_name)
+
+    return render(request, 'shopping-lists.html', {
+        'shopping_items': list_of_shopping_items
     })
