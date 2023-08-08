621af70c984f3eefb4835fa79959c81d5af2a2f4
patmull <patmull@seznam.cz>
2023-08-08 13:13:24 +0200

    Added the function of booking passengers

--- AirlineProject/flights/urls.py
+++ AirlineProject/flights/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
 from flights import views
 
 app_name = 'flights'
 urlpatterns = [
     path('', views.index, name='flight_homepage'),
     path('<str:flight_number>', views.flight, name='flight_detail'),
+    path('<str:flight_number>/book', views.book, name='book_flight'),
 ]
--- AirlineProject/flights/views.py
+++ AirlineProject/flights/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-from flights.models import Flight
+from flights.models import Flight, Passenger
 
 
 # Create your views here.
@@ -12,8 +14,23 @@ def index(request):
 
 def flight(request, flight_number):
     filtered_flight = Flight.objects.filter(flight_number=flight_number).first()
+    passengers = Passenger.objects.all()
     return render(request, 'flights/flight-detail.html',
                   {
                       'flight': filtered_flight,
-                      'passengers': filtered_flight.passengers.all()
+                      'flight_passengers': filtered_flight.passengers.all(),
+                      'passengers': passengers
                   })
+
+
+def book(request, flight_number):
+    print("HERE in book method")
+    if request.method == "POST":
+        passenger_id = request.POST.get('passenger_id')
+        passenger_found = Passenger.objects.filter(id=passenger_id).first()
+        flight_number = request.POST.get('flight_number')
+        flight_found = Flight.objects.filter(flight_number=flight_number).first()
+
+        flight_found.passengers.add(passenger_found)
+
+        return HttpResponseRedirect(reverse('flights:flight_detail', args=[flight_found.flight_number]))
