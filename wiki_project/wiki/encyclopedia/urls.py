b4f6686976c83aefbbc44ef8f52bf8b83027789c
patmull <patmull@seznam.cz>
2023-08-09 00:06:49 +0200

    Added search

--- wiki_project/wiki/encyclopedia/urls.py
+++ wiki_project/wiki/encyclopedia/urls.py
@@ -4,6 +4,7 @@ import encyclopedia.util
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path('wiki/<str:title>', views.title, name="title")
+    path('', views.index, name="index"),
+    path('wiki/<str:title>', views.title, name="title"),
+    path('search', views.search_encyclopedia, name='search_encyclopedia')
 ]
--- wiki_project/wiki/encyclopedia/views.py
+++ wiki_project/wiki/encyclopedia/views.py
@@ -22,3 +22,25 @@ def title(request, title):
                       {
                           'entry_detail': entry_detail
                       })
+
+
+def search_encyclopedia(request):
+    searched_text = request.GET['q']
+    entry_text = util.get_entry(searched_text)
+
+    if entry_text is not None:
+        entry_detail = {'title': searched_text, 'text': entry_text}
+        return render(request, "encyclopedia/entry-detail.html",
+                      {
+                          'entry_detail': entry_detail
+                      })
+    else:
+        entries = util.list_entries()
+        print(entries)
+        print(searched_text)
+        matched_entries = [entry for entry in entries if str.lower(searched_text) in (str.lower(entry))]
+        print(matched_entries)
+        return render(request, "encyclopedia/search-results.html",
+                      {
+                          'search_results': matched_entries
+                      })
