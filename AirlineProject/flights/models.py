1824a1f5cf9c0a426686570b37d85a842730779f
patmull <patmull@seznam.cz>
2023-08-08 09:50:33 +0200

    Added ManyToMany relationships and tried that with Python commands

--- AirlineProject/flights/models.py
+++ AirlineProject/flights/models.py
@@ -9,14 +9,23 @@ class Airport(models.Model):
     def __str__(self):
         return f"City: {self.city}, street: {self.street}, # of runways: {self.number_of_runways}"
 
-
 # Create your models here.
+
+
 class Flight(models.Model):
     flight_number = models.CharField(max_length=20)
     duration = models.IntegerField()
-    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='airport_origin')
-    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='airport_destination')
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='flight_airport_origin')
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='flight_airport_destination')
 
     def __str__(self):
         return (f"Flight number: {self.flight_number}, duration: {self.duration}, origin: {self.origin}"
                 f", destination: {self.destination}")
+
+
+class Passenger(models.Model):
+    name = models.CharField(max_length=64)
+    flights = models.ManyToManyField(Flight, blank=True, related_name='passengers_flights')
+
+    def __str__(self):
+        return f"{ self.name }"
