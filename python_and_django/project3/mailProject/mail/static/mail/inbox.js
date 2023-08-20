7ed71b0f573b0c17dddc8dc942fdbebe67101b10
patmull <patmull@seznam.cz>
2023-08-20 12:24:01 +0200

    CReating e-mail detail

--- python_and_django/project3/mailProject/mail/static/mail/inbox.js
+++ python_and_django/project3/mailProject/mail/static/mail/inbox.js
@@ -8,11 +8,17 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#compose').addEventListener('click', compose_email);
 
   // Later created elements: This cannot contain class name, because it does not exist the page (DOM) load!
-  document.addEventListener('click', (event) => load_email_detail(event));
+  document.getElementById('emails-view')
+      .addEventListener('click', (event) => load_email_detail(event));
 
   // By default, load the inbox
   load_mailbox('inbox');
 });
+
+function array_to_string(array_to_convert)
+{
+  return array_to_convert.toString();
+}
 
 function compose_email() {
 
@@ -76,6 +82,7 @@ function load_mailbox(mailbox) {
 function load_email_detail(event)
 {
   event.preventDefault();
+
   const target_link = event.target.closest('.email-link');
   console.log("target_link:");
   console.log(target_link);
@@ -84,21 +91,75 @@ function load_email_detail(event)
   console.log(email_id);
   const email_detail = fetch_email_detail(email_id);
 
+  const emails_view = document.querySelector('#emails-view');
+
   email_detail.then(loaded_email => {
     // Access the resolved value (array of emails)
-
 
     // Access individual email objects
     //
     if (!(Object.keys(loaded_email).length === 0)) {
       // loaded_email.forEach(loaded_email);
-      console.log("Loaded e-mail passed the check:");
+      console.log("E-mail loaded succesfully.");
       console.log(loaded_email);
+
+      create_email_detail(loaded_email);
     }
   }).catch(error => {
     // NOTICE: We can send also a console.error to the console
     console.error("An error occurred:", error);
   });
+
+}
+
+function removeChildElements(element)
+{
+  console.log("Removed element:");
+  console.log(element);
+  element.remove();
+}
+
+function create_email_detail(email)
+{
+  function create_email_header_info_item(info_type, info_text)
+  {
+    const email_header_info = document.createElement('div');
+    const email_header_info_type = document.createElement('span');
+
+    email_header_info.innerText = `${info_text}`;
+    email_header_info_type.innerText = `${info_type}: `;
+    email_header_info_type.className = 'email-info-label';
+
+    email_header_info.appendChild(email_header_info_type);
+
+    return email_header_info;
+  }
+
+  const from_email_header_info = create_email_header_info_item('From', email.sender);
+  const recipient_string = array_to_string(email.recipients);
+  const to_email_header_info = create_email_header_info_item('To', recipient_string);
+  const subject_email_header_info = create_email_header_info_item('Subject', email.subject);
+  const timestamp_email_header_info = create_email_header_info_item('Timestamp', email.timestamp);
+
+  const reply_button = document.createElement('button');
+  reply_button.innerText = 'Reply';
+
+  const email_view = document.querySelector('#emails-view');
+
+  // clearing the email_view
+  email_view.replaceChildren();
+
+  const email_text_element = document.createElement('div');
+  email_text_element.innerText = email.body;
+
+  const separating_line = document.createElement('hr');
+
+  email_view.appendChild(from_email_header_info);
+  email_view.appendChild(to_email_header_info);
+  email_view.appendChild(subject_email_header_info);
+  email_view.appendChild(timestamp_email_header_info);
+  email_view.appendChild(separating_line);
+  email_view.appendChild(email_text_element);
 
 }
 
