59b48e7c9d085931ed6f00bb430da9407fb6e43b
patmull <patmull@seznam.cz>
2023-08-24 14:04:17 +0200

    Loading follwing users

--- complex_web_projects/project4/network/static/network/scripts.js
+++ complex_web_projects/project4/network/static/network/scripts.js
@@ -27,6 +27,8 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
+    document.querySelector('#following-nav-link').addEventListener('click', (event) => load_followings());
+
     document.querySelector('#posts').display = 'block';
     load_posts();
 });
@@ -123,16 +125,7 @@ function load_posts() {
     {
         if(!isEmpty(logged_in_user_name))
         {
-            // User is logged in
-            const loaded_following_promise = fetch_following();
-
-            loaded_following_promise.then(
-                loaded_followers => {
-                  following = loaded_followers.following;
-                  console.log("Following:");
-                  console.log(following);
-                }
-            )
+            following = get_following_users();
         }
     }
 
@@ -182,49 +175,53 @@ function load_posts() {
 
             if(following === null || following === undefined)
             {
-                console.error("Followers variable is undefined, please contact admin of the app to solve this issue.");
+                console.log("No followers found.");
             } else
             {
-                if(following.includes(loaded_post.user_name))
-                {
-                    console.log("User already followed.");
-                    console.log("Post user:");
-                    console.log(loaded_post.user_name);
-                    console.log("Logged user is following:");
-                    console.log(following);
-                    already_following = true;
-                    include_follow_unfollow_form = true;
-                }
-                console.log("Followers:");
-                console.log(following);
+                following
+                    .then(following => {
+                        if(following.includes(loaded_post.user_name))
+                        {
+                            console.log("User already followed.");
+                            console.log("Post user:");
+                            console.log(loaded_post.user_name);
+                            console.log("Logged user is following:");
+                            console.log(following);
+                            already_following = true;
+                            include_follow_unfollow_form = true;
+                        }
+                        console.log("Following:");
+                        console.log(following);
+
+                        const new_text = document.createElement('p');
+                        new_text.innerText = loaded_post.text;
+                        const new_date = document.createElement('p');
+                        new_date.className = 'post-date';
+                        new_date.innerText = loaded_post.created_at;
+
+                        let follow_form;
+                        if(include_follow_unfollow_form === true)
+                        {
+                            console.log(`include_follow_unfollow_form: ${include_follow_unfollow_form}`);
+                            if(already_following === true)
+                            {
+                                follow_form = create_follow_unfollow_form("Unfollow", loaded_post.user_id);
+                                post_div.appendChild(follow_form);
+                            } else {
+                                follow_form = create_follow_unfollow_form("Follow", loaded_post.user_id);
+                                post_div.appendChild(follow_form);
+                            }
+                        }
+
+                        post_div.appendChild(new_text);
+                        post_div.appendChild(new_date);
+
+                        const posts_list = document.querySelector('#posts-list');
+                        posts_list.appendChild(post_div);
+
+                        posts_section_selector.appendChild(posts_list);
+                    })
             }
-
-            const new_text = document.createElement('p');
-            new_text.innerText = loaded_post.text;
-            const new_date = document.createElement('p');
-            new_date.className = 'post-date';
-            new_date.innerText = loaded_post.created_at;
-
-            let follow_form;
-            if(include_follow_unfollow_form === true)
-            {
-                if(already_following === true)
-                {
-                    follow_form = create_follow_unfollow_form("Unfollow", loaded_post.user_id);
-                    post_div.appendChild(follow_form);
-                } else {
-                    follow_form = create_follow_unfollow_form("Follow", loaded_post.user_id);
-                    post_div.appendChild(follow_form);
-                }
-            }
-
-            post_div.appendChild(new_text);
-            post_div.appendChild(new_date);
-
-            const posts_list = document.querySelector('#posts-list');
-            posts_list.appendChild(post_div);
-
-            posts_section_selector.appendChild(posts_list);
         }
 
         if(loaded_posts.length > 0)
@@ -253,6 +250,46 @@ function load_posts() {
     });
 }
 
+
+function get_following_users()
+{
+    // User is logged in
+    const loaded_following_promise = fetch_following();
+
+    return loaded_following_promise.then(
+        loaded_followers => {
+          return loaded_followers.following;
+        }
+    )
+}
+
+function load_followings() {
+
+    const posts_section = document.getElementById('posts');
+
+    function append_following_user(user_name)
+    {
+        console.log("user_name (following):");
+        console.log(user_name);
+
+        posts_section.append(`<p>${user_name}</p>`);
+    }
+
+    document.getElementById('page-title').innerText = "Following";
+
+    posts_section.innerText = '';
+
+    const followings_promise = get_following_users();
+
+    followings_promise.then(following => {
+        console.log("following:");
+        console.log(following);
+
+        following.forEach(append_following_user);
+    })
+
+}
+
 function fetch_posts_api()
 {
     return fetch('/all-posts', {method: 'GET'})
@@ -266,8 +303,8 @@ function fetch_following()
 {
     return fetch('/following', {method: 'GET'})
         .then(response => response.json())
-        .then(following => {
-            return following;
+        .then(data => {
+            return data;
         })
 }
 
